f9d6a33cfb71d113e9c03c6b26a644eefa350d33
Gichinsan <durstdennis@gmail.com>
2021-04-16 13:24:54 +0200

    Added comments added API documentation

--- README.md
+++ README.md
@@ -1,7 +1,596 @@
 # myGermanHolidays
-MyGermanHolidays ist in der Lage Feste und Bewegliche Feiertage aus Deutschland zu berechnen.
-Man kann einzelne Feiertage abfragen oder sich pro Monat die Arbeitstage wiedergeben lassen.
 
+MyGermanHolidays ist in der Lage Feste und bewegliche Feiertage aus Deutschland zu berechnen. Man kann einzelne
+Feiertage abfragen oder sich pro Monat die Arbeitstage wiedergeben lassen. Alle Abfragen sind REST abfragen.
 
 **Anmerkung:**
-Es handelt sich um Gesetzliche Feiertage in Deutschland
+
+Es handelt sich um gesetzliche Feiertage innerhalb Deutschlands.
+
+## Deploy to development server
+
+mvnw wagon:upload-single@upload-to-myserver
+
+## API Dokumentation
+
+---
+
+## **Aschermittwoch**
+
+---
+
+* **URL**
+
+  `/v1/aschermittwoch`
+
+* **Method:**
+
+  `GET`
+
+* **URL Params**
+
+  `value = "year"`
+
+* **Data Params**
+
+  `None`
+
+* **Success Response:**
+
+* **Code:** 200 <br />
+  **Content:** `2021-02-17T23: 00`
+
+* **Error Response:**
+
+* **Code:** 404 Not Found <br />
+  **Content:** `Not Found`
+
+* **Sample Call:**
+
+  ```http://localhost:8889/v1/aschermittwoch?year=2021```
+
+## **Palmsonntag**
+
+---
+
+* **URL**
+
+  `/v1/palmsonntag`
+
+* **Method:**
+
+  `GET`
+
+* **URL Params**
+
+  `value = "year"`
+
+* **Data Params**
+
+  `None`
+
+* **Success Response:**
+
+* **Code:** 200 <br />
+  **Content:** `2021-03-27T23: 00`
+
+* **Error Response:**
+
+* **Code:** 404 Not Found <br />
+  **Content:** `Not Found`
+
+* **Sample Call:**
+
+  ```http://localhost:8889/v1/palmsonntag?year=2021```
+
+## **Karfreitag**
+
+---
+
+* **URL**
+
+  `/v1/karfreitag`
+
+* **Method:**
+
+  `GET`
+
+* **URL Params**
+
+  `value = "year"`
+
+* **Data Params**
+
+  `None`
+
+* **Success Response:**
+
+* **Code:** 200 <br />
+  **Content:** `2021-04-02T00: 00`
+
+* **Error Response:**
+
+* **Code:** 404 Not Found <br />
+  **Content:** `Not Found`
+
+* **Sample Call:**
+
+  ```http://localhost:8889/v1/karfreitag?year=2021```
+
+## **Ostersonntag**
+
+---
+
+* **URL**
+
+  `/v1/ostersonntag`
+
+* **Method:**
+
+  `GET`
+
+* **URL Params**
+
+  `value = "year"`
+
+* **Data Params**
+
+  `None`
+
+* **Success Response:**
+
+* **Code:** 200 <br />
+  **Content:** `2021-04-04T00: 00`
+
+* **Error Response:**
+
+* **Code:** 404 Not Found <br />
+  **Content:** `Not Found`
+
+* **Sample Call:**
+
+  ```http://localhost:8889/v1/ostersonntag?year=2021```
+
+## **Ostermontag**
+
+---
+
+* **URL**
+
+  `/v1/ostermontag`
+
+* **Method:**
+
+  `GET`
+
+* **URL Params**
+
+  `value = "year"`
+
+* **Data Params**
+
+  `None`
+
+* **Success Response:**
+
+* **Code:** 200 <br />
+  **Content:** `2021-04-05T00: 00`
+
+* **Error Response:**
+
+* **Code:** 404 Not Found <br />
+  **Content:** `Not Found`
+
+* **Sample Call:**
+
+  ```http://localhost:8889/v1/ostermontag?year=2021```
+
+## **ChristiHimmelfahrt**
+
+---
+
+* **URL**
+
+  `/v1/christiHimmelfahrt`
+
+* **Method:**
+
+  `GET`
+
+* **URL Params**
+
+  `value = "year"`
+
+* **Data Params**
+
+  `None`
+
+* **Success Response:**
+
+* **Code:** 200 <br />
+  **Content:** `2021-05-13T00: 00`
+
+* **Error Response:**
+
+* **Code:** 404 Not Found <br />
+  **Content:** `Not Found`
+
+* **Sample Call:**
+
+  ```http://localhost:8889/v1/christiHimmelfahrt?year=2021```
+
+## **Pfingsten**
+
+---
+
+* **URL**
+
+  `/v1/pfingsten`
+
+* **Method:**
+
+  `GET`
+
+* **URL Params**
+
+  `value = "year"`
+
+* **Data Params**
+
+  `None`
+
+* **Success Response:**
+
+* **Code:** 200 <br />
+  **Content:** `2021-05-24T00: 00`
+
+* **Error Response:**
+
+* **Code:** 404 Not Found <br />
+  **Content:** `Not Found`
+
+* **Sample Call:**
+
+  ```http://localhost:8889/v1/pfingsten?year=2021```
+
+## **Fronleichnam**
+
+---
+
+* **URL**
+
+  `/v1/fronleichnam`
+
+* **Method:**
+
+  `GET`
+
+* **URL Params**
+
+  `value = "year"`
+
+* **Data Params**
+
+  `None`
+
+* **Success Response:**
+
+* **Code:** 200 <br />
+  **Content:** `2021-06-03T00: 00`
+
+* **Error Response:**
+
+* **Code:** 404 Not Found <br />
+  **Content:** `Not Found`
+
+* **Sample Call:**
+
+  ```http://localhost:8889/v1/fronleichnam?year=2021```
+
+## **MaiFeiertage**
+
+---
+
+* **URL**
+
+  `/v1/maifeiertag`
+
+* **Method:**
+
+  `GET`
+
+* **URL Params**
+
+  `value = "year"`
+
+* **Data Params**
+
+  `None`
+
+* **Success Response:**
+
+* **Code:** 200 <br />
+  **Content:** `2021-05-01`
+
+* **Error Response:**
+
+* **Code:** 404 Not Found <br />
+  **Content:** `Not Found`
+
+* **Sample Call:**
+
+  ```http://localhost:8889/v1/maifeiertag?year=2021```
+
+## **Arbeitstage**
+
+---
+
+* **URL**
+
+  `/v1/calcDayofMonth`
+
+* **Method:**
+
+  `GET`
+
+* **URL Params**
+
+    * `value = "year"`
+    * `value = "month"`
+
+
+* **Data Params**
+
+  `None`
+
+* **Success Response:**
+
+* **Code:** 200 <br />
+  **Content:** `19`
+
+* **Error Response:**
+
+* **Code:** 404 Not Found <br />
+  **Content:** `Not Found`
+
+* **Sample Call:**
+
+  ```http://localhost:8889/v1/calcDayofMonth?year=2021&month=1```
+
+## **HeiligeDreiKönige**
+
+---
+
+* **URL**
+
+  `/v1/heilige`
+
+* **Method:**
+
+  `GET`
+
+* **URL Params**
+
+  `value = "year"`
+
+* **Data Params**
+
+  `None`
+
+* **Success Response:**
+
+* **Code:** 200 <br />
+  **Content:** `2021-01-06`
+
+* **Error Response:**
+
+* **Code:** 404 Not Found <br />
+  **Content:** `Not Found`
+
+* **Sample Call:**
+
+  ```http://localhost:8889/v1/heilige?year=2021```
+
+## **Neujahr**
+
+---
+
+* **URL**
+
+  `/v1/neujahr`
+
+* **Method:**
+
+  `GET`
+
+* **URL Params**
+
+  `value = "year"`
+
+* **Data Params**
+
+  `None`
+
+* **Success Response:**
+
+* **Code:** 200 <br />
+  **Content:** `2021-01-01
+
+* **Error Response:**
+
+* **Code:** 404 Not Found <br />
+  **Content:** `Not Found`
+
+* **Sample Call:**
+
+  ```http://localhost:8889/v1/neujahr?year=2021```
+
+## **Tag der Deutschen Einheit**
+
+---
+
+* **URL**
+
+  `/v1/tagderDeutschenEinheit`
+
+* **Method:**
+
+  `GET`
+
+* **URL Params**
+
+  `value = "year"`
+
+* **Data Params**
+
+  `None`
+
+* **Success Response:**
+
+* **Code:** 200 <br />
+  **Content:** `2021-10-03`
+
+* **Error Response:**
+
+* **Code:** 404 Not Found <br />
+  **Content:** `Not Found`
+
+* **Sample Call:**
+
+  ```http://localhost:8889/v1/tagderDeutschenEinheit?year=2021```
+
+## **Weihnachten**
+
+---
+
+* **URL**
+
+  `/v1/weihnachten`
+
+* **Method:**
+
+  `GET`
+
+* **URL Params**
+
+  `value = "year"`
+
+* **Data Params**
+
+  `None`
+
+* **Success Response:**
+
+* **Code:** 200 <br />
+  **Content:** `2021-12-24`
+
+* **Error Response:**
+
+* **Code:** 404 Not Found <br />
+  **Content:** `Not Found`
+
+* **Sample Call:**
+
+  ```http://localhost:8889/v1/weihnachten?year=2021```
+
+## **1. Weihnachtsfeiertag**
+
+---
+
+* **URL**
+
+  `/v1/1Weihnachten`
+
+* **Method:**
+
+  `GET`
+
+* **URL Params**
+
+  `value = "year"`
+
+* **Data Params**
+
+  `None`
+
+* **Success Response:**
+
+* **Code:** 200 <br />
+  **Content:** `2021-12-25`
+
+* **Error Response:**
+
+* **Code:** 404 Not Found <br />
+  **Content:** `Not Found`
+
+* **Sample Call:**
+
+  ```http://localhost:8889/v1/1Weihnachten?year=2021```
+
+## **2. Weihnachtsfeiertag**
+
+---
+
+* **URL**
+
+  `/v1/2Weihnachten`
+
+* **Method:**
+
+  `GET`
+
+* **URL Params**
+
+  `value = "year"`
+
+* **Data Params**
+
+  `None`
+
+* **Success Response:**
+
+* **Code:** 200 <br />
+  **Content:** `2021-12-26`
+
+* **Error Response:**
+
+* **Code:** 404 Not Found <br />
+  **Content:** `Not Found`
+
+* **Sample Call:**
+
+  ```http://localhost:8889/v1/2Weihnachten?year=2021```
+
+## **Silvester**
+
+---
+
+* **URL**
+
+  `/v1/silvester`
+
+* **Method:**
+
+  `GET`
+
+* **URL Params**
+
+  `value = "year"`
+
+* **Data Params**
+
+  `None`
+
+* **Success Response:**
+
+* **Code:** 200 <br />
+  **Content:** `2021-12-31`
+
+* **Error Response:**
+
+* **Code:** 404 Not Found <br />
+  **Content:** `Not Found`
+
+* **Sample Call:**
+
+  ```http://localhost:8889/v1/silvester?year=2021```
--- src/main/java/de/gichinsan/myGermanHolidays/controller/MyGermanHolidayController.java
+++ src/main/java/de/gichinsan/myGermanHolidays/controller/MyGermanHolidayController.java
@@ -3,80 +3,128 @@ package de.gichinsan.myGermanHolidays.controller;
 import de.gichinsan.myGermanHolidays.utils.CalcWorkMonthlyDays;
 import de.gichinsan.myGermanHolidays.utils.FixedFeasts;
 import de.gichinsan.myGermanHolidays.utils.MovableFeasts;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.Serializable;
 
 @RestController
-@Slf4j
 public class MyGermanHolidayController implements Serializable {
 
-    @RequestMapping(value = "/v1/aschermittwoch", produces = "application/json; charset=UTF-8")
+    /**
+     * Returns the String object of the requested date of Aschermittwoch.
+     *
+     * @param year the year for a day of Aschermittwoch
+     * @return the Date of Aschermittwoch
+     */
+    @RequestMapping(value = "/v1/aschermittwoch", produces = "application/json; charset=UTF-8", method = RequestMethod.GET)
     @ResponseBody
     public String getAschermittwoch(@RequestParam(value = "year") int year) {
         MovableFeasts mf = new MovableFeasts(year);
         return mf.getAschermittwoch().toString();
     }
 
-    @RequestMapping(value = "/v1/palmsonntag", produces = "application/json; charset=UTF-8")
+    /**
+     * Returns the String object of the requested date of Palmsonntag.
+     *
+     * @param year the year for a day of Palmsonntag
+     * @return the Date of Palmsonntag
+     */
+    @RequestMapping(value = "/v1/palmsonntag", produces = "application/json; charset=UTF-8", method = RequestMethod.GET)
     @ResponseBody
     public String getPalmsonntag(@RequestParam(value = "year") int year) {
         MovableFeasts mf = new MovableFeasts(year);
         return mf.getPalmsonntag().toString();
     }
 
-    @RequestMapping(value = "/v1/karfreitag", produces = "application/json; charset=UTF-8")
+    /**
+     * Returns the String object of the requested date of Karfreitag.
+     *
+     * @param year the year for a day of Karfreitag
+     * @return the Date of Karfreitag
+     */
+    @RequestMapping(value = "/v1/karfreitag", produces = "application/json; charset=UTF-8", method = RequestMethod.GET)
     @ResponseBody
     public String getKarfreitag(@RequestParam(value = "year") int year) {
         MovableFeasts mf = new MovableFeasts(year);
         return mf.getKarfreitag().toString();
     }
 
-    @RequestMapping(value = "/v1/ostersonntag", produces = "application/json; charset=UTF-8")
+    /**
+     * Returns the String object of the requested date of Ostersonntag.
+     *
+     * @param year the year for a day of Ostersonntag
+     * @return the Date of Ostersonntag
+     */
+    @RequestMapping(value = "/v1/ostersonntag", produces = "application/json; charset=UTF-8", method = RequestMethod.GET)
     @ResponseBody
     public String getOstersonntag(@RequestParam(value = "year") int year) {
         MovableFeasts mf = new MovableFeasts(year);
         return mf.getOstersonntag().toString();
     }
 
-    @RequestMapping(value = "/v1/ostermontag", produces = "application/json; charset=UTF-8")
+    /**
+     * Returns the String object of the requested date of Ostermontag.
+     *
+     * @param year the year for a day of Ostermontag
+     * @return the Date of Ostermontag
+     */
+    @RequestMapping(value = "/v1/ostermontag", produces = "application/json; charset=UTF-8", method = RequestMethod.GET)
     @ResponseBody
     public String getOstermontag(@RequestParam(value = "year") int year) {
         MovableFeasts mf = new MovableFeasts(year);
         return mf.getOstermontag().toString();
     }
 
-    @RequestMapping(value = "/v1/christiHimmelfahrt", produces = "application/json; charset=UTF-8")
+    /**
+     * Returns the String object of the requested date of ChristiHimmelfahrt.
+     *
+     * @param year the year for a day of ChristiHimmelfahrt
+     * @return the Date of ChristiHimmelfahrt
+     */
+    @RequestMapping(value = "/v1/christiHimmelfahrt", produces = "application/json; charset=UTF-8", method = RequestMethod.GET)
     @ResponseBody
     public String getChristiHimmelfahrt(@RequestParam(value = "year") int year) {
         MovableFeasts mf = new MovableFeasts(year);
         return mf.getChristiHimmelfahrt().toString();
     }
 
-    @RequestMapping(value = "/v1/pfingsten", produces = "application/json; charset=UTF-8")
+    /**
+     * Returns the String object of the requested date of Pfingsten.
+     *
+     * @param year the year for a day of Pfingsten
+     * @return the Date of Pfingsten
+     */
+    @RequestMapping(value = "/v1/pfingsten", produces = "application/json; charset=UTF-8", method = RequestMethod.GET)
     @ResponseBody
     public String getPfingsten(@RequestParam(value = "year") int year) {
         MovableFeasts mf = new MovableFeasts(year);
         return mf.getPfingsten().toString();
     }
 
-    @RequestMapping(value = "/v1/fronleichnam", produces = "application/json; charset=UTF-8")
+    /**
+     * Returns the String object of the requested date of Fronleichnam.
+     *
+     * @param year the year for a day of Fronleichnam
+     * @return the Date of Fronleichnam
+     */
+    @RequestMapping(value = "/v1/fronleichnam", produces = "application/json; charset=UTF-8", method = RequestMethod.GET)
     @ResponseBody
     public String getFronleichnam(@RequestParam(value = "year") int year) {
         MovableFeasts mf = new MovableFeasts(year);
         return mf.getFronleichnam().toString();
     }
 
-
-    @RequestMapping(value = "/v1/maifeiertag", produces = "application/json; charset=UTF-8")
+    /**
+     * Returns the String object of the requested date of MaiFeiertage.
+     *
+     * @param year the year for a day of MaiFeiertage
+     * @return the Date of MaiFeiertage
+     */
+    @RequestMapping(value = "/v1/maifeiertag", produces = "application/json; charset=UTF-8", method = RequestMethod.GET)
     @ResponseBody
     public String getMaiFeiertage(@RequestParam(value = "year") int year) {
         FixedFeasts ff = new FixedFeasts(year);
-        return ff.getsMaifeiertag().toString();
+        return ff.getsMaifeiertag();
     }
 
     /**
@@ -86,61 +134,102 @@ public class MyGermanHolidayController implements Serializable {
      * @param month INT
      * @return Anzahl der Arbeitstage INT
      */
-    @RequestMapping(value = "/v1/calcDayofMonth", produces = "application/json; charset=UTF-8")
+    @RequestMapping(value = "/v1/calcDayofMonth", produces = "application/json; charset=UTF-8", method = RequestMethod.GET)
     @ResponseBody
     public int getCalcDayofMonth(@RequestParam(value = "year") int year, @RequestParam(value = "month") int month) {
         CalcWorkMonthlyDays cm = new CalcWorkMonthlyDays(year, month);
         return cm.getCalcWorkMonthlyDays();
     }
 
-    @RequestMapping(value = "/v1/heilige", produces = "application/json; charset=UTF-8")
+    /**
+     * Returns the String object of the requested date of HeiligeDreiKönige.
+     *
+     * @param year the year for a day of HeiligeDreiKönige
+     * @return the Date of HeiligeDreiKönige
+     */
+    @RequestMapping(value = "/v1/heilige", produces = "application/json; charset=UTF-8", method = RequestMethod.GET)
     @ResponseBody
     public String getsHeilige(@RequestParam(value = "year") int year) {
         FixedFeasts ff = new FixedFeasts(year);
-        return ff.getsHeilige().toString();
+        return ff.getsHeilige();
     }
 
-    @RequestMapping(value = "/v1/neujahr", produces = "application/json; charset=UTF-8")
+    /**
+     * Returns the String object of the requested date of Neujahr.
+     *
+     * @param year the year for a day of Neujahr
+     * @return the Date of Neujahr
+     */
+    @RequestMapping(value = "/v1/neujahr", produces = "application/json; charset=UTF-8", method = RequestMethod.GET)
     @ResponseBody
     public String getsNeujahr(@RequestParam(value = "year") int year) {
         FixedFeasts ff = new FixedFeasts(year);
-        return ff.getsNeujahr().toString();
+        return ff.getsNeujahr();
     }
 
-    @RequestMapping(value = "/v1/tagderDeutschenEinheit", produces = "application/json; charset=UTF-8")
+    /**
+     * Returns the String object of the requested date of Tag der Deutschen Einheit.
+     *
+     * @param year the year for a day of Tag der Deutschen Einheit
+     * @return the Date of Tag der Deutschen Einheit
+     */
+    @RequestMapping(value = "/v1/tagderDeutschenEinheit", produces = "application/json; charset=UTF-8", method = RequestMethod.GET)
     @ResponseBody
     public String getsTagderDeutschenEinheit(@RequestParam(value = "year") int year) {
         FixedFeasts ff = new FixedFeasts(year);
-        return ff.getsTagderDeutschenEinheit().toString();
+        return ff.getsTagderDeutschenEinheit();
     }
 
-    @RequestMapping(value = "/v1/weihnachten", produces = "application/json; charset=UTF-8")
+    /**
+     * Returns the String object of the requested date of Weihnachten.
+     *
+     * @param year the year for a day of Weihnachten
+     * @return the Date of Weihnachten
+     */
+    @RequestMapping(value = "/v1/weihnachten", produces = "application/json; charset=UTF-8", method = RequestMethod.GET)
     @ResponseBody
     public String getsWeihnachten(@RequestParam(value = "year") int year) {
         FixedFeasts ff = new FixedFeasts(year);
-        return ff.getsWeihnachten().toString();
+        return ff.getsWeihnachten();
     }
 
-    @RequestMapping(value = "/v1/1Weihnachten", produces = "application/json; charset=UTF-8")
+    /**
+     * Returns the String object of the requested date of 1. Weihnachtsfeiertag.
+     *
+     * @param year the year for a day of 1. Weihnachtsfeiertag
+     * @return the Date of 1. Weihnachtsfeiertag
+     */
+    @RequestMapping(value = "/v1/1Weihnachten", produces = "application/json; charset=UTF-8", method = RequestMethod.GET)
     @ResponseBody
     public String gets1Weihnachten(@RequestParam(value = "year") int year) {
         FixedFeasts ff = new FixedFeasts(year);
-        return ff.gets1Weihnachten().toString();
+        return ff.gets1Weihnachten();
     }
 
-    @RequestMapping(value = "/v1/2Weihnachten", produces = "application/json; charset=UTF-8")
+    /**
+     * Returns the String object of the requested date of 2. Weihnachtsfeiertag.
+     *
+     * @param year the year for a day of 2. Weihnachtsfeiertag
+     * @return the Date of 2. Weihnachtsfeiertag
+     */
+    @RequestMapping(value = "/v1/2Weihnachten", produces = "application/json; charset=UTF-8", method = RequestMethod.GET)
     @ResponseBody
     public String gets2Weihnachten(@RequestParam(value = "year") int year) {
         FixedFeasts ff = new FixedFeasts(year);
-        return ff.gets2Weihnachten().toString();
+        return ff.gets2Weihnachten();
     }
 
-    @RequestMapping(value = "/v1/silvester", produces = "application/json; charset=UTF-8")
+    /**
+     * Returns the String object of the requested date of Silvester.
+     *
+     * @param year the year for a day of Silvester
+     * @return the Date of Silvester
+     */
+    @RequestMapping(value = "/v1/silvester", produces = "application/json; charset=UTF-8", method = RequestMethod.GET)
     @ResponseBody
     public String getsSilvester(@RequestParam(value = "year") int year) {
         FixedFeasts ff = new FixedFeasts(year);
-        return ff.getsSilvester().toString();
+        return ff.getsSilvester();
     }
-
 
 }
